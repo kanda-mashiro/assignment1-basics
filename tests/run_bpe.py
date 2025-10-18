eaeb4194649d9f817271ba0d8b667599aa96c106
o7si <o7si@kanda-mashiro.cc>
2025-10-18 22:37:08 +0800

    bug fix

--- tests/run_bpe.py
+++ tests/run_bpe.py
@@ -1,4 +1,16 @@
 from adapters import run_train_bpe_v1, run_train_bpe_v2
+from common import gpt2_bytes_to_unicode
 
-_, _ = run_train_bpe_v1("./tests/fixtures/tinystories_sample_5M.txt", 300, ["<|endoftext|>"])
-_, _ = run_train_bpe_v2("./tests/fixtures/tinystories_sample_5M.txt", 300, ["<|endoftext|>"])
+# v1, m1 = run_train_bpe_v1("./tests/fixtures/tinystories_sample_5M.txt", 300, ["<|endoftext|>"])
+# v2, m2 = run_train_bpe_v2("./tests/fixtures/tinystories_sample_5M.txt", 300, ["<|endoftext|>"])
+v1, m1 = run_train_bpe_v1("./tests/fixtures/corpus.en", 300, ["<|endoftext|>"])
+# v2, m2 = run_train_bpe_v2("./tests/fixtures/corpus.en", 500, ["<|endoftext|>"])
+print(v1)
+for k, v in v1.items():
+    print(k, v)
+
+# assert v1 == v2
+# assert v2 == v3
+# print(v2)
+# print(v3)
+
